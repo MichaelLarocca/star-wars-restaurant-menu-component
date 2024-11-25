b69b4e89679cf3972c492544c40b6a22f792267f
Michael Larocca <michaellarocca@live.com>
2024-11-24 19:21:20 -0500

    Added isVertical to display text vertical

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,30 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
-import stylesAurebesh from "./components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.module.css";
+// import stylesAurebesh from "./components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.module.css";
 import AurebeshTranslatorComponent from "./components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx";
-import starWarsImage from "./components/AurebeshTranslatorComponent/StarWars.png";
+// import starWarsImage from "./components/AurebeshTranslatorComponent/StarWars.png";
 
 function App() {
-  return (
-    <>
-      <h1>Aurebesh Translator</h1>
-
-      <div className="App">
-        <AurebeshTranslatorComponent
-          text={`obi wan cannoli: $4.99`}
-          scale={0.5}
-          isWhite={false}
-        />
-      </div>
-      <div className="App">
-        <AurebeshTranslatorComponent
-          text={`caff: $1`}
-          scale={0.5}
-          isWhite={false}
-        />
-      </div>
-
-      {/* <div
-        className={stylesAurebesh.a}
-        style={{
-          backgroundImage: `url(${starWarsImage})`,
-          backgroundPosition: "-8px -145px",
-        }}
-      >
-        a
-      </div>
-      <div
-        className={stylesAurebesh.b}
-        style={{
-          backgroundImage: `url(${starWarsImage})`,
-          backgroundPosition: "-62px -145px",
-        }}
-      >
-        b
-      </div>
-      <div
-        className={stylesAurebesh.c}
-        style={{
-          backgroundImage: `url(${starWarsImage})`,
-          backgroundPosition: "-115px -145px",
-        }}
-      >
-        c
-      </div>
-      <div
-        className={stylesAurebesh.Tt}
-        style={{
-          backgroundImage: `url(${starWarsImage})`,
-          backgroundPosition: "-323px -405px",
-        }}
-      >
-        Tt
-      </div> */}
-    </>
-  );
+	return (
+		<>
+			<h1>Aurebesh Translator</h1>
+			<div className="App">
+				<AurebeshTranslatorComponent
+					text={`obi wan cannoli: $4.99`}
+					scale={0.5}
+					isWhite={false}
+					isVertical={false}
+				/>
+			</div>
+			<div className="App">
+				<AurebeshTranslatorComponent
+					text={`caff: $1`}
+					scale={0.5}
+					isWhite={false}
+					isVertical={false}
+				/>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx
+++ src/components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import { aurebeshTranslator } from "../../utils/aurebeshTranslator.jsx";
 
-const AurebeshTranslatorComponent = ({
-  text,
-  scale = 1,
-  isWhite = false,
-  ...rest
-}) => {
-  return (
-    // <div style={{ display: "flex", alignItems: "center" }}>
-    //   {aurebeshTranslator(text, scale, isWhite)}
-    // </div>
-    <div style={{ display: "flex", alignItems: "center" }}>
-      {aurebeshTranslator(text, scale, isWhite)}
-    </div>
-  );
-};
+function AurebeshTranslatorComponent({
+	text,
+	scale = 1,
+	isWhite = false,
+	isVertical = false,
+	...rest
+}) {
+	const [vertical, setVertical] = useState(isVertical);
+
+	return (
+		<div style={{ display: vertical ? "block" : "flex", alignItems: "center" }}>
+			{aurebeshTranslator(text, scale, isWhite, vertical)}
+		</div>
+	);
+}
 
 export default AurebeshTranslatorComponent;
