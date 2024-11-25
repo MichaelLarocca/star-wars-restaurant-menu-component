03be253034bc852cebda64998d1cf09a955745c0
Larocca <Larocca@RIVPRO1.net>
2024-11-25 12:19:32 -0500

    Feature: Added vertical text

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@ import AurebeshTranslatorComponent from "./components/AurebeshTranslatorComponen
 // import starWarsImage from "./components/AurebeshTranslatorComponent/StarWars.png";
 
 function App() {
-	return (
-		<>
-			<h1>Aurebesh Translator</h1>
-			<div className="App">
-				<AurebeshTranslatorComponent
-					text={`obi wan cannoli: $4.99`}
-					scale={0.5}
-					isWhite={false}
-					isVertical={false}
-					isAurebesh={true}
-				/>
-			</div>
-			<div className="App">
-				<AurebeshTranslatorComponent
-					text={`caff: $1`}
-					scale={0.5}
-					isWhite={false}
-					isVertical={true}
-					isAurebesh={true}
-				/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <h1>Aurebesh Translator</h1>
+      <div className="App">
+        <AurebeshTranslatorComponent
+          text={`obi wan cannoli: $4.99`}
+          scale={1}
+          isWhite={false}
+          isVertical={false}
+          isAurebesh={true}
+        />
+      </div>
+      <div className="App">
+        {/* <AurebeshTranslatorComponent
+          text={`caff: $1`}
+          scale={0.5}
+          isWhite={false}
+          isVertical={true}
+          isAurebesh={true}
+        /> */}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx
+++ src/components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 import { aurebeshTranslator } from "../../utils/aurebeshTranslator.jsx";
+// import cssFile from "./AurebeshTranslatorComponent.module.css";
+import cssFile from "../AurebeshTranslatorComponent/AurebeshTranslatorComponent.module.css";
 
 function AurebeshTranslatorComponent({
-	text,
-	scale = 1,
-	isWhite = false,
-	isVertical = false,
-	isAurebesh = true,
-	...rest
+  text,
+  scale = 1,
+  isWhite = false,
+  isVertical = false,
+  isAurebesh = true,
+  ...rest
 }) {
-	const [vertical, setVertical] = useState(isVertical);
-	const [aurebesh, setAurebesh] = useState(isAurebesh);
+  const [vertical, setVertical] = useState(isVertical);
+  const [aurebesh, setAurebesh] = useState(isAurebesh);
 
-	return (
-		<div style={{ display: vertical ? "block" : "flex", alignItems: "center" }}>
-			{aurebesh ? aurebeshTranslator(text, scale, isWhite, vertical) : text}
-		</div>
-	);
+  return (
+    <div
+      style={{ display: vertical ? "block" : "flex", alignItems: "center" }}
+      className={vertical && !isAurebesh ? cssFile.verticalText : ""}
+    >
+      {aurebesh ? aurebeshTranslator(text, scale, isWhite, vertical) : text}
+    </div>
+  );
 }
 
 export default AurebeshTranslatorComponent;
