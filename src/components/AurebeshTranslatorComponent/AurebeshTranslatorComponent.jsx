c0ae0a608bfcc46659ddabaaa1be9154283264cd
Michael Larocca <michaellarocca@live.com>
2025-04-24 13:02:05 -0400

    Added comments to components

--- src/components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx
+++ src/components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx
@@ -2,30 +2,64 @@ import { useState } from "react";
 import { aurebeshTranslator } from "../../utils/aurebeshTranslator.jsx";
 import cssFile from "../AurebeshTranslatorComponent/AurebeshTranslatorComponent.module.css";
 
+// AurebeshTranslatorComponent
+// This component translates English text to Aurebesh and displays it with customizable styles.
 function AurebeshTranslatorComponent({
-  data,
-  isAurebesh = true,
-  scale = 1,
-  isWhite = false,
-  isVertical = false,
-  verticalLetterSpacing = "-7px",
-  horizontalLetterSpacing = "10px",
-  fontSize = "16px"
-  
+  data, // The text to be translated
+  isAurebesh = true, // Flag to determine if the text should be translated to Aurebesh
+  scale = 1, // Scale factor for the size of the translated text
+  isWhite = false, // Flag to determine if the text color should be white
+  isVertical = false, // Flag to determine if the text should be displayed vertically
+  verticalLetterSpacing = "-7px", // Letter spacing for vertical text
+  horizontalLetterSpacing = "10px", // Letter spacing for horizontal text
+  fontSize = "16px" // Font size of the text
 }) {
   return (
     <div
       className={`${cssFile.aurebeshTranslator} ${isVertical && !isAurebesh ? cssFile.verticalText : ""}`}
       style={{ 
-        display: isVertical ? "block" : "flex", 
+        display: isVertical ? "block" : "flex", // Display style based on text orientation
         alignItems: "center",
-        letterSpacing: isVertical ? verticalLetterSpacing : horizontalLetterSpacing,
-        fontSize: fontSize,
+        letterSpacing: isVertical ? verticalLetterSpacing : horizontalLetterSpacing, // Apply appropriate letter spacing
+        fontSize: fontSize, // Set font size
        }}
     >
-      {isAurebesh ? aurebeshTranslator(data, scale, isWhite, isVertical) : data}
+      {isAurebesh ? aurebeshTranslator(data, scale, isWhite, isVertical) : data} {/* Render translated or original text */}
     </div>
   );
 }
 
 export default AurebeshTranslatorComponent;
+
+// import { useState } from "react";
+// import { aurebeshTranslator } from "../../utils/aurebeshTranslator.jsx";
+// import cssFile from "../AurebeshTranslatorComponent/AurebeshTranslatorComponent.module.css";
+
+// function AurebeshTranslatorComponent({
+//   data,
+//   isAurebesh = true,
+//   scale = 1,
+//   isWhite = false,
+//   isVertical = false,
+//   verticalLetterSpacing = "-7px",
+//   horizontalLetterSpacing = "10px",
+//   fontSize = "16px"
+  
+// }) {
+//   return (
+//     <div
+//       className={`${cssFile.aurebeshTranslator} ${isVertical && !isAurebesh ? cssFile.verticalText : ""}`}
+//       style={{ 
+//         display: isVertical ? "block" : "flex", 
+//         alignItems: "center",
+//         letterSpacing: isVertical ? verticalLetterSpacing : horizontalLetterSpacing,
+//         fontSize: fontSize,
+//        }}
+//     >
+//       {isAurebesh ? aurebeshTranslator(data, scale, isWhite, isVertical) : data}
+//     </div>
+//   );
+// }
+
+// export default AurebeshTranslatorComponent;
+
