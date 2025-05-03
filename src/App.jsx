abd76faf40b7378bb8a5288e5b86dbee6dd6550a
Michael Larocca <michaellarocca@live.com>
2025-05-03 11:41:44 -0400

    Working model

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import useDarkMode from "./utils/useDarkMode";
 import AurebeshTranslatorComponent from "./components/AurebeshTranslatorComponent/AurebeshTranslatorComponent.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Anakin from "/Anakin.png";
 import "./App.css";
 
 function App() {
@@ -29,20 +30,103 @@ function App() {
         isVertical={isVertical}
       />
 
-      <h1>Aurebesh Translator</h1>
+      <div>
+        <AurebeshTranslatorComponent
+          data={`Menu`} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={1.5} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={false} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"50px"} // Font size of the text
+        />
+        </div>
 
-      {/* AurebeshTranslatorComponent with various props for customization */}
-      <AurebeshTranslatorComponent
-        data={`may the 4th be with you!`} // Text to be translated
-        isAurebesh={isAurebesh} // Toggle for Aurebesh translation
-        scale={.5} // Scale of the text
-        isWhite={isDarkMode} // Text color based on dark mode
-        isVertical={isVertical} // Text orientation
-        verticalLetterSpacing="-7px" // Letter spacing for vertical text
-        horizontalLetterSpacing="10px" // Letter spacing for horizontal text
-        fontSize={"16px"} // Font size of the text
-      />
-    </div>
+      <img src={Anakin} alt="Anakin Skywalker"  style={{ width: '300px', height: 'auto', display: 'flex', flexWrap: 'wrap' }} />
+
+      <div>
+        <em>
+        <AurebeshTranslatorComponent
+          data={`"this is where the fun begins!"`} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={.4} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={false} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"16px"} // Font size of the text
+        />
+        </em>
+        </div>
+
+      <div className="menu-container">
+        <div>
+        <AurebeshTranslatorComponent
+          data={`*appetizers*`} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={1} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={true} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"30px"} // Font size of the text
+        />
+        </div>
+        <div style={{ transform: 'translateX(-50px)' }}>
+        <AurebeshTranslatorComponent
+          data={`$12 - obi wan pierogis`} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={.5} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={false} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"16px"} // Font size of the text
+        />
+        <AurebeshTranslatorComponent
+          data={`$10 - greedo’s burritos`} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={.5} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={false} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"16px"} // Font size of the text
+        />
+        <AurebeshTranslatorComponent
+          data={`$9 - tusken raider taters `} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={.5} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={false} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"16px"} // Font size of the text
+        />
+        <AurebeshTranslatorComponent
+          data={`$14 - bantha wings`} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={.5} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={false} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"16px"} // Font size of the text
+        />        
+        <AurebeshTranslatorComponent
+          data={`$8 - wookiee wontons`} // Text to be translated
+          isAurebesh={isAurebesh} // Toggle for Aurebesh translation
+          scale={.5} // Scale of the text
+          isWhite={isDarkMode} // Text color based on dark mode
+          isVertical={false} // Text orientation
+          verticalLetterSpacing="-7px" // Letter spacing for vertical text
+          horizontalLetterSpacing="10px" // Letter spacing for horizontal text
+          fontSize={"16px"} // Font size of the text
+        />        
+        </div>
+      </div>
+      </div>
   );
 }
 
